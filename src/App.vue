8037d458f3a4fd457942cf223ae236ead2e5f0da
Lo Yu-Cheng <david855033@gmail.com>
2018-06-19 23:51:35 +0800

    .

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div class="form-group">
+      <label for="ic">IC卡號</label>
+      <select class="form-control" id="ic" size='5'>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label for="ic">健兒門診身分</label>
+      <select class="form-control" id="ic" size='5'>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label for="bh">身高</label>
+      <input type="number" class="form-control" id="bh">
+    </div>
+    <div class="form-group">
+      <label for="bw">體重</label>
+      <input type="number" class="form-control" id="bw">
+    </div>
+    <div class="form-group">
+      <label for="bt">體溫</label>
+      <input type="number" class="form-control" id="bt">
+    </div>
+     <div class="form-group">
+      <label for="s">S</label>
+      <textarea type="number" class="form-control" id="s" rows="10"></textarea>
+    </div>
+    <div class="form-group">
+      <label for="o">O</label>
+      <textarea type="number" class="form-control" id="o" rows="10"></textarea>
+    </div>
+    <div class="form-group">
+      <label for="p">P</label>
+      <textarea type="number" class="form-control" id="p" rows="10"></textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+@import "./assets/custom.scss";
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
+</style>
